b2a6dd18694412251acb5279da9053a156d516dd
GabbyMcCarthy <gah1289@gmail.com>
2023-01-03 14:34:48 -0500

    changed db.js file

--- db.js
+++ db.js
@@ -1,35 +1,35 @@
-'use strict';
-/** Database setup for jobly. */
-const { Client } = require('pg');
-const { getDatabaseUri } = require('./config');
-
-let db;
-
-if (process.env.NODE_ENV === 'production') {
-	db = new Client({
-		connectionString : getDatabaseUri(),
-		ssl              : {
-			rejectUnauthorized : false
-		}
-	});
-}
-else {
-	db = new Client({
-		connectionString : getDatabaseUri()
-	});
-}
-
-db.connect();
-
-module.exports = db;
-
+// 'use strict';
+// /** Database setup for jobly. */
 // const { Client } = require('pg');
 // const { getDatabaseUri } = require('./config');
-// const db = new Client({
-// 	connectionString : getDatabaseUri(),
-// 	ssl              : {
-// 		rejectUnauthorized : false
-// 	}
-// });
+
+// let db;
+
+// if (process.env.NODE_ENV === 'production') {
+// 	db = new Client({
+// 		connectionString : getDatabaseUri(),
+// 		ssl              : {
+// 			rejectUnauthorized : false
+// 		}
+// 	});
+// }
+// else {
+// 	db = new Client({
+// 		connectionString : getDatabaseUri()
+// 	});
+// }
+
 // db.connect();
+
 // module.exports = db;
+
+const { Client } = require('pg');
+const { getDatabaseUri } = require('./config');
+const db = new Client({
+	connectionString : getDatabaseUri(),
+	ssl              : {
+		rejectUnauthorized : false
+	}
+});
+db.connect();
+module.exports = db;
