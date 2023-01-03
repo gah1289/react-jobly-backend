7ee48cfda02fdd8fb4af008e74c1cb12f9cbb07d
GabbyMcCarthy <gah1289@gmail.com>
2023-01-03 14:10:42 -0500

    changed db.js file

--- db.js
+++ db.js
@@ -1,22 +1,29 @@
-"use strict";
+'use strict';
 /** Database setup for jobly. */
-const { Client } = require("pg");
-const { getDatabaseUri } = require("./config");
+const { Client } = require('pg');
+const { getDatabaseUri } = require('./config');
 
-let db;
+const db = new Client({
+	connectionString : getDatabaseUri(),
+	ssl              : {
+		rejectUnauthorized : false
+	}
+});
 
-if (process.env.NODE_ENV === "production") {
-  db = new Client({
-    connectionString: getDatabaseUri(),
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
-} else {
-  db = new Client({
-    connectionString: getDatabaseUri()
-  });
-}
+// let db;
+
+// if (process.env.NODE_ENV === "production") {
+//   db = new Client({
+//     connectionString: getDatabaseUri(),
+//     ssl: {
+//       rejectUnauthorized: false
+//     }
+//   });
+// } else {
+//   db = new Client({
+//     connectionString: getDatabaseUri()
+//   });
+// }
 
 db.connect();
 
