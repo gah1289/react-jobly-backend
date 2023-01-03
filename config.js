bf3eb16841e3a2bcd8b211bc3f08b0fbf4a89fd1
GabbyMcCarthy <gah1289@gmail.com>
2023-01-03 16:19:37 -0500

    checking port

--- config.js
+++ config.js
@@ -9,6 +9,8 @@ const SECRET_KEY = process.env.SECRET_KEY || 'secret-dev';
 
 const PORT = process.env.PORT || 3001;
 
+console.log('***********PORT', PORT);
+
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
 	return process.env.NODE_ENV === 'test' ? 'jobly_test' : process.env.DATABASE_URL || 'jobly';
